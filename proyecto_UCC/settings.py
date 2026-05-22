86696f1474500846e0fcea6841d126537790d7bf
Daniel Pereira <danielpereiralopez3@gmail.com>
2026-05-22 03:32:45 -0500

    fix email port

--- proyecto_UCC/settings.py
+++ proyecto_UCC/settings.py
@@ -104,9 +104,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'  
 EMAIL_HOST          = 'smtp.gmail.com'
-EMAIL_PORT          = 587
-EMAIL_USE_TLS       = True
-EMAIL_USE_SSL       = False
+EMAIL_PORT          = 465
+EMAIL_USE_TLS       = False
+EMAIL_USE_SSL       = True
 EMAIL_TIMEOUT       = 30
 EMAIL_HOST_USER     = os.environ.get('EMAIL_HOST_USER', 'danielpereiralopez3@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'zhag psjc umpy zmjj')
